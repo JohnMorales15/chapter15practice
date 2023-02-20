7b72ffe22ef2eb3f3d5950d2d25d9cce855573c4
JohnMorales15 <119957569+JohnMorales15@users.noreply.github.com>
2023-02-20 15:28:19 -0700

    created dinoEggController

--- src/main/java/org/launchcode/Ch15practice/controllers/EggController.java
+++ src/main/java/org/launchcode/Ch15practice/controllers/EggController.java
@@ -1,16 +1,28 @@
 package org.launchcode.Ch15practice.controllers;
 
 
+import org.launchcode.Ch15practice.data.DinoEggRepository;
+import org.launchcode.Ch15practice.data.DinosaurRepository;
 import org.launchcode.Ch15practice.models.DinoEgg;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("egg")
 public class EggController {
+    @Autowired
+    private DinosaurRepository dinoRepository;
+    @Autowired
+    private DinoEggRepository dinoEggRepository;
 
     @GetMapping
     public String egg(){
@@ -20,11 +32,22 @@ public class EggController {
     @GetMapping("add")
     public String displayAddEggForm(Model model){
         model.addAttribute("dinoEgg", new DinoEgg());
+        model.addAttribute("allDinos", dinoRepository.findAll());
+
         return "egg/add";
     }
 
     @PostMapping("add")
-    public String processAddEggForm(){
-        return "egg";
+    public String processAddEggForm(Model model, @ModelAttribute @Valid DinoEgg newdinoEgg,
+                                    Errors errors){
+
+        if(errors.hasErrors()){
+            model.addAttribute("allDinos", dinoRepository.findAll());
+            return "egg/add";
+        }
+
+        dinoEggRepository.save(newdinoEgg);
+
+        return "redirect: ";
     }
 }
